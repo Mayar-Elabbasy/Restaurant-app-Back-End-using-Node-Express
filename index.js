c08e5665d0b8f54161265b89e32609fa3f6399dd
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-12-17 15:48:40 +0200

    Express Router & dishRouter main structure

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -21,5 +27,4 @@ const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
     console.log(`The Server is running at http://${hostname}:${port}`);
-})
-
+});
